9fae4c521285f1e6328bb13a21ad47f3ee045f65
tankiograce-afk <tankiograce@gmail.com>
2025-10-02 11:15:34 +0100

    Add files via upload

---
+++ liste_double_circulaire.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Définition de la structure d'un nœud de la liste
+typedef struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+// Fonction pour créer un nouveau nœud
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Erreur d'allocation mémoire.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+// Fonction pour insérer un élément en tête de la liste
+void insertHead(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;
+        newNode->prev = *head;
+    } else {
+        Node* temp = (*head)->prev;
+        temp->next = newNode;
+        newNode->prev = temp;
+        newNode->next = *head;
+        (*head)->prev = newNode;
+        *head = newNode;
+    }
+}
+
+// Fonction pour insérer un élément en queue de la liste
+void insertTail(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;
+        newNode->prev = *head;
+    } else {
+        Node* temp = (*head)->prev;
+        temp->next = newNode;
+        newNode->prev = temp;
+        newNode->next = *head;
+        (*head)->prev = newNode;
+    }
+}
+
+// Fonction pour afficher la liste
+void printList(Node* head) {
+    if (head == NULL) {
+        printf("Liste vide.\n");
+        return;
+    }
+    Node* temp = head;
+    do {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("(retour à la tête)\n");
+}
+
+// Fonction pour libérer la mémoire de la liste
+void freeList(Node** head) {
+    if (*head == NULL) {
+        return;
+    }
+    Node* temp = *head;
+    Node* next;
+    do {
+        next = temp->next;
+        free(temp);
+        temp = next;
+    } while (temp != *head);
+    *head = NULL;
+}
+
+int main() {
+    Node* head = NULL; // Initialisation de la liste vide
+    int choix, data;
+
+    while (1) {
+        printf("Menu:\n");
+        printf("1. Insérer un élément en tête de la liste\n");
+        printf("2. Insérer un élément en queue de la liste\n");
+        printf("3. Afficher la liste\n");
+        printf("4. Quitter\n");
+        printf("Choisissez une option : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                printf("Entrez la valeur de l'élément : ");
+                scanf("%d", &data);
+                insertHead(&head, data);
+                break;
+            case 2:
+                printf("Entrez la valeur de l'élément : ");
+                scanf("%d", &data);
+                insertTail(&head, data);
+                break;
+            case 3:
+                printf("Liste actuelle : ");
+                printList(head);
+                break;
+            case 4:
+                freeList(&head);
+                return 0;
+            default:
+                printf("Option invalide. Veuillez réessayer.\n");
+        }
+    }
+
+    return 0;
+}
